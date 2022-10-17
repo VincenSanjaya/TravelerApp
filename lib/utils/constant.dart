4f9e76cc28484cc71e5e7fda020f0f70278b7bac
VincenSanjaya <vincenciogfs15@gmail.com>
2022-10-17 13:02:55 +0700

    First Commit

---
+++ lib/utils/constant.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+Color primary = const Color(0xFFF2AD54);
+
+class Styles {
+  static Color primaryColor = primary;
+  static Color white = const Color(0xFFFAF9F8);
+  static Color grey = const Color(0xFFC8C4BB);
+  static Color black = const Color(0xFF141419);
+  static Color brown = const Color(0xFF4C454B);
+  static Color darkPrimary = const Color(0xFFBE9256);
+  static Color darkerPrimary = const Color(0xFFB0743E);
+}
