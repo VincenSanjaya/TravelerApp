bb2978ce2d627e6bb96571c7a7da821155abf9af
VincenSanjaya <vincenciogfs15@gmail.com>
2022-10-24 07:59:46 +0700

    feat: [VG] Add Login Page

---
+++ lib/screens/login-screen/login-screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_traveler_app/screens/login-screen/components/body.dart';
+
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: BodyLogin(),
+      ),
+    );
+  }
+}
