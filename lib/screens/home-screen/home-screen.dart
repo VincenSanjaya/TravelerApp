2351afed58ef3982cc91564874b353f7087384c9
VincenSanjaya <vincenciogfs15@gmail.com>
2022-10-27 19:34:42 +0700

    Feat: [VG] Connect the register

--- lib/screens/home-screen/home-screen.dart
+++ lib/screens/home-screen/home-screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_traveler_app/screens/home-screen/components/body.dart';
 
 class HomeScreen extends StatefulWidget {
-  static String routeName = "/loginscreen";
+  static String routeName = "/homescreen";
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
